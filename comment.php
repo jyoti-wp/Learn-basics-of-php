a4cccc5a6a254cca6fcd297c16becd687e36fad0
jyoti-wp <sim_s006@icloud.com>
2021-06-05 11:52:44 +0530

    first commit

---
+++ comment.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <!-- It's very important to document your file  -->
+    <!-- Let's learn how to add comments in php  -->
+
+    <!-- To write comment in php you start with 2 forward slashes -->
+
+    <?php
+
+    //  This is comment 
+
+    /* And if its more than on line comment
+    heres what you need to do
+    start with forward slash and astric sign and end with astric and forward slash
+    */
+
+    ?>
+</body>
+
+</html>
---
+++ title.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Title Example</title>
+</head>
+
+<body>
+    <!-- if we are willing to create dynamic data. means we just change the data on one page and it gets updated on the rest. heres how you do it  -->
+
+    <!-- lets say title -->
+    <!-- so we create a variable -->
+    <?php $title = "Jyoti Shukla's Official site"; ?>
+
+    <h2 class="title"><?php echo $title; ?> </h2>
+
+</body>
+
+</html>

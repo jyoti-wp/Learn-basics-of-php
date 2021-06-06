b1c273ade5a8516ec707dfcc472ecd1b555500b3
jyoti-wp <sim_s006@icloud.com>
2021-06-06 11:03:12 +0530

    switch statment update

---
+++ switch-statement.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Switch statement</title>
+</head>
+
+<body>
+    <?php
+    $number = 34;
+
+    switch() {
+
+        case 43:
+        echo "it is 34";
+        break;
+        case 29:
+        echo "it is 34";
+        break;
+        case 14:
+        echo "it is 34";
+        break;
+    }
+    ?>
+</body>
+
+</html>

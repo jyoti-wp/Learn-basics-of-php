15c3434fb4a95b9d48beef23fbaad6d8a44fa1c1
jyoti-wp <sim_s006@icloud.com>
2021-06-06 11:19:54 +0530

    switch statement final update

--- switch-statement.php
+++ switch-statement.php
@@ -12,19 +12,27 @@
     <?php
     $number = 34;
 
-    switch() {
+    switch ($number) {
 
         case 43:
-        echo "it is 34";
-        break;
+            echo "it is 34";
+            break;
         case 29:
-        echo "it is 34";
-        break;
+            echo "it is 34";
+            break;
         case 14:
-        echo "it is 34";
-        break;
+            echo "it is 34";
+            break;
+
+            // if all the cases are not true  we write default
+
+        default:
+            echo "we could not match anything";
     }
     ?>
+
+
+    <!-- make sure to add break in between for the statement to break out once its found the answer -->
 </body>
 
 </html>

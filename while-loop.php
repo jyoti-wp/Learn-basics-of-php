0bd6f19d92cf0c00e58cc55fbd5306b5014f343d
jyoti-wp <sim_s006@icloud.com>
2021-06-06 11:33:02 +0530

    while loop update

---
+++ while-loop.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>while loop</title>
+</head>
+
+<body>
+    <!-- while loops can run infinite.
+
+    let's see an example -->
+    <?php
+    $i = 10;
+
+    while ($i < 20) {
+
+        echo "<h2>Hello Motherfuckers</h2>";
+        echo "<br>";
+        $i++;
+    }
+
+    ?>
+</body>
+
+</html>

44f4d89153c7c327b534c81f0d05cf98d3a4d7f9
jyoti-wp <sim_s006@icloud.com>
2021-06-06 10:48:04 +0530

    switch statement basic

---
+++ comparison-operator.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>comparison operator</title>
+</head>
+
+<body>
+
+    <!-- comparison operator 
+equal ==
+identical ===
+compare > < >= <= <>
+not equal !=
+not identical !==
+assigment =
+ -->
+
+
+    <!-- logical operator
+ And &&
+ or ||
+ not !
+ 
+  -->
+    <?php ?>
+</body>
+
+</html>

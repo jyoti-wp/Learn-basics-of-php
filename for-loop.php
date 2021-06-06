423ccd025fc35fbc7aac401de4e19bf49d659c55
jyoti-wp <sim_s006@icloud.com>
2021-06-06 12:58:44 +0530

    for loop update

---
+++ for-loop.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>for loop</title>
+</head>
+
+<body>
+    <!-- while loops and for loops are not really different form each other. You can decide
+    if you wanna use while loop or for loop coz they
+    both can help you get the same outcome  -->
+</body>
+
+</html>

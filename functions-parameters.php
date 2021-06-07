b3b25a92875a0e0f005494877cc0a58bdd75bb2f
jyoti-wp <sim_s006@icloud.com>
2021-06-07 15:57:57 +0530

    function parameter update

---
+++ functions-parameters.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Functions with parameters</title>
+</head>
+
+<body>
+    <!-- we already know how to define a function. Now we will learn how -->
+    <!-- to pass parameters -->
+
+    <!-- we pass is a parameter inside of the paranthese
+and we pass the argument when we call the function -->
+
+    <?php
+    function greet($super)
+    {
+        echo $super;
+    }
+
+    greet('<h1>Man Of Steel</h1>');
+    ?>
+</body>
+
+</html>

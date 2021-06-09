f1d6ca8bdac30ebd4fdebec91749be0034a3990a
jyoti-wp <sim_s006@icloud.com>
2021-06-09 09:29:46 +0530

    constants update

---
+++ constants.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Constants</title>
+</head>
+
+<body>
+    <!-- ther are pre defined function available in php  
+define is one of them. Lets check few examples of define
+you can also write constants in const form
+be informed that once defines cannot be changed-->
+
+    <?php
+    define($name, 28);  // define the function and give it a value and just echo the function
+    echo $name;
+    //  now if we try changing this value it wont be possible
+    //  can constants once defined cannot be changes 
+
+    ?>
+</body>
+
+</html>
